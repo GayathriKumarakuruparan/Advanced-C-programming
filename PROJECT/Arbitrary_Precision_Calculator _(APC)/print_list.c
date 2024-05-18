82015fc6e380af636c971e646d51ff4db5569032
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-05-18 13:30:45 +0530

    Add files via upload

---
+++ PROJECT/Arbitrary_Precision_Calculator _(APC)/print_list.c
@@ -0,0 +1,32 @@
+/*
+Name: Gayathri.k
+Date:
+Description:
+Sample input:
+Sample output:
+*/
+#include"apc.h"
+void print_list(Dlist *head)
+{
+    if(head==NULL)
+    {
+	printf("!!!!!!!!LIST IS EMPTY!!!!!!!");
+    }
+    else
+    {
+	while(head)
+	{
+	if(head->data <10)
+	    printf("000");
+	else if(head->data > 9 && head->data <100)
+	    printf("00");
+	else if(head->data >99 && head->data < 1000)
+	    printf("0");
+	printf("%d",head->data);
+	head=head->next;
+	}
+	//return SUCCESS;
+	putchar('\n');
+    }
+}
+
